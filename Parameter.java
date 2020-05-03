a6b1d40a86521c86ded1325caa905cb5d8243421
Mady-Camara <64424083+Mady-Camara@users.noreply.github.com>
2020-05-03 19:03:39 +0000

    GDSM_PART2

---
+++ Parameter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BDD;
+
+/**
+ *
+ * @author Seno4
+ */
+public class Parameter {
+
+    public static String IPHOST = "localhost";
+    public static String HOST_DB = "jdbc:mysql://"+IPHOST+":3306/Inscription";
+    public static String USERNAME_DB = "root";
+    public static String PASSWORD_DB = "Iloveinformatique";//"root"
+    //public static String PORT="11111";
+    //public static String USER;
+    public static int PORT = 11111;
+    public static String USER;
+}
